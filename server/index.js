8e9f3bb89bfd8605e929d5eb584495703503376d
SolRobot <Altair_SA@mail.ru>
2025-12-26 16:45:05 +0300

    HTTP-запросы

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 let express = require('express')
 let app = express()
 
-app.get('/', function(request, response){
-    response.send('Hello, Node.js')
+app.get('/folder/page', function(request, response){
+    let x = request.query.foo
+    response.send({foo: x})
 })
 
 app.listen(591)
